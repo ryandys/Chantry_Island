6a07fd37c4f65bd7bcb183904d43d315ff04cc99
ryandys <ryan@ourexpressionstudio.com>
2017-02-05 11:45:35 -0500

    completed volunteers
    volunteers page completed

--- volunteers.php
+++ volunteers.php
@@ -15,6 +15,34 @@ include_once("layout/header.php");
 			</div>
 		</div>
 	</div>
+	
+	<div id="volWrapper">
+		<div class="row" data-equalizer>
+			<div class="small-12 medium-4 columns">
+				<div class="volCon" data-equalizer-watch>
+					<h1>Don Nicholson</h1>
+					<h2>Chairman</h2>
+					<p>Don is the chairman of the organization and has a great passion for Chantry Island and preserving its beauty.</p>
+				</div>
+			</div>
+			
+			<div class="small-12 medium-4 columns">
+				<div class="volCon" data-equalizer-watch>
+					<h1>Pat O'Connor</h1>
+					<h2>Vice Chairman</h2>
+					<p>As vice chairman of Chantry Island and Marine Heritage Society, Pat has a love for the group and the task.</p>
+				</div>
+			</div>
+			
+			<div class="small-12 medium-4 columns">
+				<div class="volCon" data-equalizer-watch>
+					<h1>John Rigby</h1>
+					<h2>Treasurer</h2>
+					<p>Arguably one of the most important jobs in the organization, John handles many of the daily tasks.</p>
+				</div>
+			</div>
+		</div>
+	</div>
     
     
     <!--CLOSE BODY CONTENT HERE-->
