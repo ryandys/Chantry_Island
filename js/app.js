2288d1ac3d7f04648266678ff7cd536bb18f41fb
ryandys <ryan@ourexpressionstudio.com>
2017-03-07 21:55:34 -0500

    much progress
    animations, side social media info and more

--- js/app.js
+++ js/app.js
@@ -2,6 +2,18 @@ $(document).foundation();
 (function() {
 	"use strict";
 
+	var offCanvasIcon = document.querySelector("#sideSocIconsCon");
 
+	function activateSociconsSide() {
+		var screenPOS;
+		screenPOS = window.scrollY;
+		 
+		if(screenPOS>100) {
+			TweenMax.to(offCanvasIcon, .5, {marginLeft: 10});
+			window.removeEventListener("scroll", activateSociconsSide, false);
+		}
+	}
+
+	window.addEventListener("scroll", activateSociconsSide, false);
 
 })();
