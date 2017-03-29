e2ad32933d9bc0c1c7889af2ccee6f251890cc71
ryandys <ryan@ourexpressionstudio.com>
2017-03-28 20:14:17 -0400

    everything from DB
    all pages that have DB content are now pulling it in

--- events.php
+++ events.php
@@ -1,4 +1,16 @@
 <?php
+	
+	//ini_set('display_errors',1);
+    //error_reporting(E_ALL);
+	
+	require_once('admin/phpscripts/init.php');
+
+	$tbl_events = "tbl_events";
+	$getEvents = getAll($tbl_events);
+	
+?>
+
+<?php
 $strPageTitle = 'Events';
 include_once("layout/header.php");
 ?>
@@ -19,29 +31,25 @@ include_once("layout/header.php");
 	<div id="eventsWrapper">
 		<div class="row" data-equalizer>
 			
-			<div class="small-12 medium-4 columns">
-				<div class="eventsCon" data-equalizer-watch>
-					<h1>Doug Johnsons 91st Birthday</h1>
-					<h2>January 10th, 2017</h2>
-					<p>Doug is one of the oldest supporters of the Chantry Island project.</p>
-				</div>
-			</div>
-			
-			<div class="small-12 medium-4 columns">
-				<div class="eventsCon" data-equalizer-watch>
-					<h1>Marine Heritage Society / Propeller Club AGM 2016</h1>
-					<h2>September 30th, 2016</h2>
-					<p>The Annual Marine Heritage Society and Propeller Club AGM will take place on Friday, November 25, 2016. Location will be at the Walker House in Southampton. Roast Beef or Chicken are the choices for food. Tickets will be on sale at the Propeller Club meetings and through Marine Heritage Board members. Continue to check postings for further information.</p>
-				</div>
-			</div>
-			
-			<div class="small-12 medium-4 columns">
-				<div class="eventsCon" data-equalizer-watch>
-					<h1>Christmas Parade Southampton 2016</h1>
-					<h2>December 2nd, 2016</h2>
-					<p>The Southampton Christmas Parade always takes place on the First Friday in December. We will be preparing the Peerless II for this annual tradition. Decoration and theme will take place in the Southampton Boathouse the week before. Come and join us and get to be a part of a great group of volunteers sailing down High Street</p>
-				</div>
-			</div>
+
+			<?php
+
+				if(!is_string($getEvents)){
+					while($row = mysqli_fetch_array($getEvents)){
+						echo "<div class=\"small-12 medium-4 columns end\">";
+							echo "<div class=\"eventsCon\" data-equalizer-watch>";
+							echo "<h1>{$row['events_title']}</h1>";
+							echo "<h2>{$row['events_date']}</h2>";
+							echo "<p>{$row['events_content']}</p>";
+							echo "</div>";
+						echo "</div>";
+					}
+				}else{
+					echo "<p>{$getEvents}</p>";
+				}
+
+			?>
+
 			
 		</div>
 	</div>
