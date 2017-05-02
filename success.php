14863284a0caf6b472fdd02571b0eeaa4717fe01
jwmcgavin <Jordan@Jordans-MacBook-Pro.local>
2017-05-02 16:04:20 -0400

    Added success page
    successful sent email will redirect to

---
+++ success.php
@@ -0,0 +1,25 @@
+<?php
+$strPageTitle = 'Success';
+include_once("layout/header.php");
+?>
+
+<meta http-equiv="refresh" content="3;index.php"/>
+    
+    
+    <!--START BODY CONTENT HERE-->         
+    
+    
+<div id="successCon">
+	<h1>Thank you for contacting us</h1>
+	<h2>We will be in touch shortly</h2>
+    <h3>You will be redirected to the homepage in <span id="countdown"></span>.</h3>
+</div>
+    
+    
+    <!--CLOSE BODY CONTENT HERE-->
+    
+    
+<script src="js/success.js"></script>
+<?php
+	include("layout/footer.php");
+?>
